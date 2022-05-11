3b2866ee10c068ee16aa2c1b79abd1eff1a0996c
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-11 16:41:13 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int counter;
+        var answer = 0;
+
+        //Input 
+        Console.WriteLine("This program multiplies two numbers using loops.");
+        Console.WriteLine("");
+        Console.Write("Enter side a number: ");
+        counter = Convert.ToInt32(Console.ReadLine());
+
+        //Process
+        for (int addedNumber = 1; addedNumber <= counter; addedNumber++) {
+            answer += addedNumber;
+        }
+        
+        Console.WriteLine("The sum is: " + answer);
+
+        Console.WriteLine("\nDone.");
+    }
+}
